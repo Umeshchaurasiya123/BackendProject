b2b5ec250d325bc68bd44bdddd98edc5fbe8131f
Umeshchaurasiya123 <umeshc1998@gmail.com>
2025-01-16 00:21:13 +0530

    userController(Register)

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,11 +1,54 @@
 import { asyncHandler } from "../utils/asynchHandler.js";
+import {ApiError} from "../utils/ApiError.js";
+import {User} from "../models/user.model.js";
 
 
 const registerUser=asyncHandler(async (req,res)=>{
 
-    res.status(200).json({
-        message:"ok"
+    
+
+    const {fullname,email,username,password}=req.body
+    console.log("email: ",email)
+    console.log("password: ",password)
+
+    // if (fullName===""){
+    //     throw new ApiError(400,"fullname is required")
+    // }
+
+    if(
+        [fullname,email,username,password].some((field)=>{
+            field?.trim()===""
+        })
+    ) 
+    {
+        throw new ApiError(400,"fullname is required")
+
+
+    }
+
+    // check if user id alredy exists or not. 
+    // based on email or name
+
+
+    const existedUser=User.findOne({
+        $or:[{username},{email}]
     })
+
+    if(existedUser){
+        throw new ApiError(409,"User with email or username already exist")
+    }
+
+    const avatarLocalPath=req.files?.avatar[0]?path;
+    
+
+    
+    // res.status(200).json({
+    //     message:"ok"
+    // })
+
+
+
+
 
 })
 
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,7 +4,20 @@ import {registerUser} from "../controllers/user.controllers.js";
 
 const router =Router()
 
-router.route("/register").post(registerUser)
+import {upload}  from "../middlewares/multer.middleware.js";
+
+
+router.route("/register").post(
+    upload.fields([
+         {
+                name:"avatar",
+         },     maxCount:1
+         {
+                name:"coverImage",
+                maxCount:1
+         }
+    ]),
+    registerUser)
 
 // router.route("/login").post(loginUser)
 
