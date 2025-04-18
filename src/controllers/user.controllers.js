2131dcf797eea5af2958a417de7240ced35f3439
Umeshchaurasiya123 <umeshc1998@gmail.com>
2025-04-18 15:02:56 +0530

    add getWatchHistory function; implement aggregation to fetch user's watch history with video owner details

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -7,6 +7,7 @@ import {uploadOnCloudinary} from "../utils/cloudinary.js"
 import { ApiResponce } from "../utils/ApiResponce.js";
 
 import jwt from "jsonwebtoken"
+import mongoose from "mongoose";
 
 
 const generateAccessAndRefershTokens= async(userId)=>{
@@ -498,6 +499,55 @@ const  getUserChannelProfile=asyncHandler(async(req,res)=>{
 
 })
 
+const getWatchHistory=asyncHandler(async(req,res)=>{
+    const user=await User.aggregate([
+        {
+            $match:{
+                _id: new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup:{
+                from:"videos",
+                localField:"watchHistory",
+                foreignField:"_id",
+                as:"watchHistory"
+            },
+
+            pipeline:[
+            {
+                $lookup:{
+                    from:"users",
+                    localField:"owner",
+                    foreignField:"_id",
+                    as:"owner",
+                    pipeline:[
+                        {
+                            $project:{
+                                fullName:1,
+                                username:1,
+                                avatar:1
+                            }
+                        }
+                    ]
+
+                }
+
+            },
+            {
+                $addFields:{
+                    owner:{$arrayElemAt:["$owner",0]}
+                }
+            }
+        ]
+        }
+        
+    ])
+
+    return res.status(200)
+    .json(new ApiResponce(200,user[0]?.watchHistory,"Watch history fetched successfully"))
+    
+})
 
 
 
@@ -511,7 +561,10 @@ export {
     updateAccountDetails,
     updateUserAvatar,
     updateUserCoverImage,
-    getUserChannelProfile
+    getUserChannelProfile,
+    getWatchHistory,
+    getWatchHistory
+    
 }
 
 
